070e3086525dcb5d7652d92d6dd721c447990b17
Michael Jhondry <brem9090@gmail.com>
2021-11-25 12:55:43 +0800

    Health: Content & Style

--- src/pages/Health.tsx
+++ src/pages/Health.tsx
@@ -2,21 +2,37 @@ import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonApp, IonRouter
 import { IonReactRouter } from "@ionic/react-router";
 import React from "react";
 import { Route, Redirect } from "react-router";
-// import './Health.css';
+import './Health.css';
 
 const Health: React.FC = () => {
     return (
     <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle className="ion-text-center">Health</IonTitle>
+        <IonHeader className="headerStyle">
+          <IonToolbar className="title1Health">
+            <IonTitle className="ion-text-center">
+                <p>Health</p>
+            </IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">
+            <IonCard className="titleHealth">
+                <h2>- - Please Choose - -</h2>
+            </IonCard>
 
+            <IonCard color="light" routerLink="/bmi" className="bmiStyle">
+                <div className="bmi">
+                    <img src="assets/images/bmi.png" alt="" />
+                </div>
+            </IonCard>
 
+            <IonCard color="light" routerLink="/bmr" className="bmrStyle">
+                <div className="bmr">
+                    <img src="assets/images/bmr.png" alt="" />
+                </div>
+            </IonCard>
+          
         </IonContent>
-        </IonPage>
+    </IonPage>
     );
   };
   
