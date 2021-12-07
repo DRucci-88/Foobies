68cf4e2b020c48fc7dad4a8dcb55181412a88430
bnatan91 <75927317+bnatan91@users.noreply.github.com>
2021-12-08 03:53:58 +0800

    favorite

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import {IonPage} from "@ionic/react";
+import {IonCard, IonContent, IonItem, IonLabel, IonList, IonListHeader, IonPage} from "@ionic/react";
 
+import './Favorite.css';
 const Favorite: React.FC = () => {
   return(
-    <IonPage>
+      <IonPage>
+        <IonContent className="ion-padding">
+          <IonCard className="titleFav">
+            <h2>Favorite</h2>
+          </IonCard>
+          <IonList className={"background_list"}>
+            <IonListHeader> My Favorite</IonListHeader>
+            <IonItem className="itemList">
+              <img className={'images'} src={'assets/example/kids.png'}/>
+              <IonLabel className="name"> Makanan</IonLabel>
+            </IonItem>
+          </IonList>
 
-    </IonPage>
+        </IonContent>
+      </IonPage>
   )
 }
 
